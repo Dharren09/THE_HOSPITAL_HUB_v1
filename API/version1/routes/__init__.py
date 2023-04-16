c6feab6fcf35fb9ec40f534512e882585c2df477
MAKOHA DHARREN PIUS <107412228+Dharren09@users.noreply.github.com>
2023-04-16 10:41:00 +0300

    Initialize the moment

--- API/version1/routes/__init__.py
+++ API/version1/routes/__init__.py
@@ -1,3 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin//python3
+
+from flask import Flask, Blueprint
+from .routes.patients import patients_bp
+from .routes.pharmacy import pharmacy_bp
+from .routes.staff import staff_bp
+from .routes.billings_and_payments import BillingInvoice_bp
+from .routes.TeleHealth import Telehealth_bp
 
 
+api_bp = Blueprint('api', __name__)
+api_bp.register_blueprint(patients_bp)
+api_bp.register_blueprint(staff_bp)
+api_bp.register_blueprint(pharmacy_bp)
+api_bp.register_blueprint(BillingInvoice_bp)
+api_bp.register_blueprint(Telehealth_bp)
